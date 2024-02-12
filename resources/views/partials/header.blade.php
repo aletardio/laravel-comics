81a0960f924a2e3312651d72a043437eed22118c
aletardio <ale.tardio99@gmail.com>
2024-02-12 15:34:08 +0100

    Added nav bar in header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,7 +7,38 @@
                 </a>
             </div>
             <div class="col-8">
-
+                <ul>
+                    <li>
+                        <a href="#">Characters</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Movies</a>
+                    </li>
+                    <li>
+                        <a href="#">Tv</a>
+                    </li>
+                    <li>
+                        <a href="#">Games</a>
+                    </li>
+                    <li>
+                        <a href="#">Collectibles</a>
+                    </li>
+                    <li>
+                        <a href="#">Videos</a>
+                    </li>
+                    <li>
+                        <a href="#">Fans</a>
+                    </li>
+                    <li>
+                        <a href="#">News</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
