c1a3a662b927158cfa27cd3be4201b7f31f2e784
aletardio <ale.tardio99@gmail.com>
2024-02-12 16:09:49 +0100

    Added jumbotron

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,6 +18,12 @@
 <body>
     @include('partials.header')
     <main>
+        <div class="jumbotron">
+            <img class="jumbotron-image" src="{{ Vite::asset('resources/images/jumbotron.jpg') }}" alt="">
+        </div>
+        <div>
+            <h5 class="jumbotron-heading">current series</h5>
+        </div>
     </main>
     @include('partials.footer')
 </body>
