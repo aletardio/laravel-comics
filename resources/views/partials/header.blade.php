e0e977f66bf87a631da3bd6215257495948c2253
aletardio <ale.tardio99@gmail.com>
2024-02-12 15:29:45 +0100

    Created header and footer in folder partials

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <a href="/">
+                    <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="Dc">
+                </a>
+            </div>
+            <div class="col-8">
+
+            </div>
+        </div>
+    </div>
+</header>
