630e895d2231bde77479f944baf56dc6312b8899
aletardio <ale.tardio99@gmail.com>
2024-02-13 15:05:54 +0100

    Added layout with @extends and @section

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,85 +1,63 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layout.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-    @include('partials.header')
-    <main>
-        <div class="jumbotron">
-            <img class="jumbotron-image" src="{{ Vite::asset('resources/images/jumbotron.jpg') }}" alt="">
-        </div>
-        <div class="comic-card">
-            @foreach ($comics as $key => $comic)
-                @if ($key === 2)
-                    <img class="card" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
-                @endif
-            @endforeach
-        </div>
-        <div class="line-blue"></div>
-        <div class="container-fluid contents">
-            <div class="container">
-                <div class="row">
-                    <div class="col-7">
-                        @foreach ($comics as $key => $comic)
-                            @if ($key === 2)
-                                <div class="text-uppercase py-4">
-                                    <h3>{{ $comic['title'] }} </h3>
-                                </div>
-                                <div class="row availability ">
-                                    <div class="col-9">
-                                        <div class="d-flex justify-content-between">
-                                            <div class="py-3 px-2">
-                                                <span class="text-green">
-                                                    U.S. Price:
-                                                </span>
-                                                <span class="text-white fw-bold">
-                                                    {{ $comic['price'] }}
-                                                </span>
-                                            </div>
-                                            <div class="text-green fw-bold text-uppercase py-3">
-                                                Available
-                                            </div>
+@section('content')
+    <div class="jumbotron">
+        <img class="jumbotron-image" src="{{ Vite::asset('resources/images/jumbotron.jpg') }}" alt="">
+    </div>
+    <div class="comic-card">
+        @foreach ($comics as $key => $comic)
+            @if ($key === 2)
+                <img class="card" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+            @endif
+        @endforeach
+    </div>
+    <div class="line-blue"></div>
+    <div class="container-fluid contents">
+        <div class="container">
+            <div class="row">
+                <div class="col-7">
+                    @foreach ($comics as $key => $comic)
+                        @if ($key === 2)
+                            <div class="text-uppercase py-4">
+                                <h3>{{ $comic['title'] }} </h3>
+                            </div>
+                            <div class="row availability ">
+                                <div class="col-9">
+                                    <div class="d-flex justify-content-between">
+                                        <div class="py-3 px-2">
+                                            <span class="text-green">
+                                                U.S. Price:
+                                            </span>
+                                            <span class="text-white fw-bold">
+                                                {{ $comic['price'] }}
+                                            </span>
                                         </div>
-                                    </div>
-                                    <div class="col-3">
-                                        <div class="btn-available text-center text-white border-start py-3">
-                                            Check Availability
+                                        <div class="text-green fw-bold text-uppercase py-3">
+                                            Available
                                         </div>
                                     </div>
                                 </div>
-                                <div class="py-2 lh-base">
-                                    {{ $comic['description'] }}
+                                <div class="col-3">
+                                    <div class="btn-available text-center text-white border-start py-3">
+                                        Check Availability
+                                    </div>
                                 </div>
-                            @endif
-                        @endforeach
+                            </div>
+                            <div class="py-2 lh-base">
+                                {{ $comic['description'] }}
+                            </div>
+                        @endif
+                    @endforeach
+                </div>
+                <div class="col-5 py-4 d-flex flex-column justify-content-center">
+                    <div class="text-uppercase text-end fw-bold txt-adv">
+                        Advertisement
                     </div>
-                    <div class="col-5 py-4 d-flex flex-column justify-content-center">
-                        <div class="text-uppercase text-end fw-bold txt-adv">
-                            Advertisement
-                        </div>
-                        <div class="d-flex justify-content-center">
-                            <img class="adv" src="{{ Vite::asset('resources/images/adv.jpg') }}" alt="">
-                        </div>
+                    <div class="d-flex justify-content-center">
+                        <img class="adv" src="{{ Vite::asset('resources/images/adv.jpg') }}" alt="">
                     </div>
                 </div>
             </div>
         </div>
-
-    </main>
-    @include('partials.footer')
-</body>
-
-</html>
+    </div>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,69 +1,43 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layout.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-    @include('partials.header')
-    <main>
-        <div class="jumbotron">
-            <img class="jumbotron-image" src="{{ Vite::asset('resources/images/jumbotron.jpg') }}" alt="">
-        </div>
-        <div>
-            <h5 class="jumbotron-heading">current series</h5>
-        </div>
-        <div class="boxes cont-comics">
-            @foreach ($comics as $comic)
-                <div class="comics-card">
-                    <div class="img-card">
-                        <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
-                    </div>
-                    <div class="text-comics text-light"> {{ $comic['series'] }}</div>
+@section('content')
+    <div class="jumbotron">
+        <img class="jumbotron-image" src="{{ Vite::asset('resources/images/jumbotron.jpg') }}" alt="">
+    </div>
+    <div>
+        <h5 class="jumbotron-heading">current series</h5>
+    </div>
+    <div class="boxes cont-comics">
+        @foreach ($comics as $comic)
+            <div class="comics-card">
+                <div class="img-card">
+                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
                 </div>
-            @endforeach
-        </div>
-        <div class="d-flex justify-content-center">
-            <button>
-                Load More
-            </button>
-        </div>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12 content d-flex justify-content-center align-items-center">
-                    <div class="d-flex flex-row px-5">
-                        <img src="{{ Vite::asset('resources/images/buy-comics-digital-comics.png') }}"
-                            class="icons my-1" />
-                        <div class="text-uppercase text-section text-light">digital comics</div>
-                        <img src="{{ Vite::asset('resources/images/buy-comics-merchandise.png') }}"
-                            class="icons my-1" />
-                        <div class="text-uppercase text-section text-light">dc merchandise</div>
-                        <img src="{{ Vite::asset('resources/images/buy-comics-subscriptions.png') }}"
-                            class="icons my-1" />
-                        <div class="text-uppercase text-section text-light">subscription</div>
-                        <img src="{{ Vite::asset('resources/images/buy-comics-shop-locator.png') }}"
-                            class="icons my-1" />
-                        <div class="text-uppercase text-section text-light">comic shop locator</div>
-                        <img src="{{ Vite::asset('resources/images/buy-dc-power-visa.svg') }}"
-                            class="icons_visa my-1" />
-                        <div class="text-uppercase text-section text-light">dc power visa</div>
-                    </div>
+                <div class="text-comics text-light"> {{ $comic['series'] }}</div>
+            </div>
+        @endforeach
+    </div>
+    <div class="d-flex justify-content-center">
+        <button>
+            Load More
+        </button>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 content d-flex justify-content-center align-items-center">
+                <div class="d-flex flex-row px-5">
+                    <img src="{{ Vite::asset('resources/images/buy-comics-digital-comics.png') }}" class="icons my-1" />
+                    <div class="text-uppercase text-section text-light">digital comics</div>
+                    <img src="{{ Vite::asset('resources/images/buy-comics-merchandise.png') }}" class="icons my-1" />
+                    <div class="text-uppercase text-section text-light">dc merchandise</div>
+                    <img src="{{ Vite::asset('resources/images/buy-comics-subscriptions.png') }}" class="icons my-1" />
+                    <div class="text-uppercase text-section text-light">subscription</div>
+                    <img src="{{ Vite::asset('resources/images/buy-comics-shop-locator.png') }}" class="icons my-1" />
+                    <div class="text-uppercase text-section text-light">comic shop locator</div>
+                    <img src="{{ Vite::asset('resources/images/buy-dc-power-visa.svg') }}" class="icons_visa my-1" />
+                    <div class="text-uppercase text-section text-light">dc power visa</div>
                 </div>
             </div>
         </div>
-    </main>
-    @include('partials.footer')
-</body>
-
-</html>
+    </div>
+@endsection
